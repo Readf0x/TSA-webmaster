7f9c7225d20365b389d9dea5f2bef5248e684ece
Readf0x <davis.a.forsythe@gmail.com>
2023-11-30 14:44:42 -0600

    added layout & global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import "./global.scss"
+  import "@fontsource-variable/inter"
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-</svelte:head>
+  <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+</svelte:head> -->
 
 <slot />
