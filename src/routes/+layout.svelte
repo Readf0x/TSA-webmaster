c02139f61854ef1869c49c972872511acd5a5f2a
readf0x <davis.a.forsythe@gmail.com>
2024-01-30 14:49:34 -0600

    began banner element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
-  // import ThemeSwitch from "$lib/components/ThemeSwitch.svelte"
-  // import "@fontsource-variable/inter/index.css"
   import "$lib/fonts/fonts.css"
   import "./global.scss"
 </script>
 
-<div class="pad">
-  <slot />
-</div>
-
-<style>
-  .pad {
-    margin: 10px;
-  }
-</style>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,5 @@
 <script>
-  import FontTable from '$lib/components/FontTable.svelte';
+  import Banner from "$lib/components/Banner.svelte";
 </script>
 
-<h1>Heading 1 <span class="c0 si">Heading 1</span></h1>
-<h2>Heading 2 <span class="c1 si">Heading 2</span></h2>
-<h3>Heading 3 <span class="c0 si">Heading 3</span></h3>
-<h4>Heading 4 <span class="c1 si">Heading 4</span></h4>
-
-<p>
-  <span class="c0">color 1</span> <br>
-  <span class="c1 sb">color 2 in bold</span> <br>
-  <span class="c2 si">color 3 in italic</span> <br>
-  <span class="f0">font 0</span> <br>
-  <span class="f1">font 1</span> <br>
-  <span class="f2">font 2</span> <br>
-</p>
-
-<FontTable></FontTable>
+<Banner></Banner>
