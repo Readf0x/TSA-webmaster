9b9e24723611cd3d5a8365a4f88f0f6ed73e2f01
readf0x <davis.a.forsythe@gmail.com>
2024-02-05 08:18:40 -0600

    4/7 completed

---
+++ src/lib/components/Guy.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import guy from "$lib/svg/guy.svg?raw"
+  import factory from "$lib/svg/factory.svg?raw"
+</script>
+
+<div class="wrapper">
+  <h1>
+    Switching doesn’t just help save your wallet<br>
+    <i class="c2">It helps save the planet too.</i>
+  </h1>
+  <div class="art">
+    <div class="factory">{@html factory}</div>
+    <div class="guy">{@html guy}</div>
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    margin-top: 150px;
+    margin-bottom: 725px;
+    h1 {
+      text-align: center;
+    }
+    .art {
+      position: absolute;
+      justify-content: space-between;
+      left: 0;
+      right: 0;
+      display: flex;
+      .guy {
+        right: 0;
+      }
+      .factory {
+        margin-left: 5%;
+        margin-top: 5%;
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
   import Banner from "$lib/components/Banner.svelte"
   import SinglePanel from "$lib/components/SinglePanel.svelte"
   import MultiPanel from "$lib/components/MultiPanel.svelte";
+  import Guy from "$lib/components/Guy.svelte";
   import Saos from "saos"
 </script>
 
 <Saos once={true}><Banner /></Saos>
 <SinglePanel />
 <MultiPanel />
+<Guy />
